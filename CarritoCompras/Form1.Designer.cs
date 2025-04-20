3996526cafbfbd75ab8eac41695fbc0dfebdce70
Adrian Mirabal <adrian.mirabal07@gmail.com>
2025-04-20 17:12:56 -0400

    version

--- CarritoCompras/Form1.Designer.cs
+++ CarritoCompras/Form1.Designer.cs
@@ -28,83 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listShopCart = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnCalcular = new System.Windows.Forms.Button();
-            this.cboProducts = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtCodigo = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
+            listShopCart = new ListBox();
+            button1 = new Button();
+            btnCalcular = new Button();
+            cboProducts = new ComboBox();
+            label1 = new Label();
+            txtCodigo = new TextBox();
+            SuspendLayout();
             // 
             // listShopCart
             // 
-            this.listShopCart.FormattingEnabled = true;
-            this.listShopCart.ItemHeight = 20;
-            this.listShopCart.Location = new System.Drawing.Point(35, 37);
-            this.listShopCart.Name = "listShopCart";
-            this.listShopCart.Size = new System.Drawing.Size(233, 384);
-            this.listShopCart.TabIndex = 0;
+            listShopCart.FormattingEnabled = true;
+            listShopCart.ItemHeight = 15;
+            listShopCart.Location = new Point(31, 28);
+            listShopCart.Margin = new Padding(3, 2, 3, 2);
+            listShopCart.Name = "listShopCart";
+            listShopCart.Size = new Size(204, 289);
+            listShopCart.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(335, 129);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Agregar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Location = new Point(293, 97);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(82, 22);
+            button1.TabIndex = 1;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(445, 129);
-            this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(130, 29);
-            this.btnCalcular.TabIndex = 2;
-            this.btnCalcular.Text = "Calcular Precio";
-            this.btnCalcular.UseVisualStyleBackColor = true;
-            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            btnCalcular.Location = new Point(389, 97);
+            btnCalcular.Margin = new Padding(3, 2, 3, 2);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(114, 22);
+            btnCalcular.TabIndex = 2;
+            btnCalcular.Text = "Calcular Precio";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
             // 
             // cboProducts
             // 
-            this.cboProducts.FormattingEnabled = true;
-            this.cboProducts.Location = new System.Drawing.Point(335, 84);
-            this.cboProducts.Name = "cboProducts";
-            this.cboProducts.Size = new System.Drawing.Size(240, 28);
-            this.cboProducts.TabIndex = 3;
+            cboProducts.FormattingEnabled = true;
+            cboProducts.Location = new Point(293, 63);
+            cboProducts.Margin = new Padding(3, 2, 3, 2);
+            cboProducts.Name = "cboProducts";
+            cboProducts.Size = new Size(210, 23);
+            cboProducts.TabIndex = 3;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(346, 188);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Codigo de Descuento";
+            label1.AutoSize = true;
+            label1.Location = new Point(303, 141);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Codigo de Descuento";
             // 
             // txtCodigo
             // 
-            this.txtCodigo.Location = new System.Drawing.Point(505, 185);
-            this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(125, 27);
-            this.txtCodigo.TabIndex = 5;
+            txtCodigo.Location = new Point(442, 139);
+            txtCodigo.Margin = new Padding(3, 2, 3, 2);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(110, 23);
+            txtCodigo.TabIndex = 5;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtCodigo);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cboProducts);
-            this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listShopCart);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 338);
+            Controls.Add(txtCodigo);
+            Controls.Add(label1);
+            Controls.Add(cboProducts);
+            Controls.Add(btnCalcular);
+            Controls.Add(button1);
+            Controls.Add(listShopCart);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Cart";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
